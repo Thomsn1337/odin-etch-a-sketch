2be924117df38c19d4fe1d7de37271e926ab51ad
Thomas Steifler <steiflerthomas@gmail.com>
2023-08-20 18:51:22 +0200

    Add basic styling and drawing function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-let gridSize = 50;
+let gridSize = 20;
+let mouseState = false;
+const defaultColor = "#e3dde2";
 
 const gridContainer = document.querySelector(".grid-container");
 
@@ -7,9 +9,14 @@ function generateGrid() {
   for (let i = 0; i < gridSize ** 2; i++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid");
+    gridItem.addEventListener("mousemove", function () {
+      if (mouseState) this.style.backgroundColor = defaultColor;
+    });
 
     gridContainer.appendChild(gridItem);
   }
 }
 
 window.addEventListener("load", generateGrid);
+window.addEventListener("mousedown", () => (mouseState = true));
+window.addEventListener("mouseup", () => (mouseState = false));
